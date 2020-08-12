e9667a5421c2dd3cde15af6a32526542db629875
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-08-12 12:00:24 +0200

    feat: 🎸 Adding <File /> component

---
+++ src/renderer/components/File/file.types.js
@@ -0,0 +1,41 @@
+import { 
+    faFilePdf, 
+    faFileWord, 
+    faFileExcel,
+    faFilePowerpoint,
+    faFileCsv,
+    faCopy,
+} from '@fortawesome/free-solid-svg-icons'
+
+export default {
+    'pdf': {
+        title: 'pdf',
+        icon: faFilePdf,
+        color: '#ff6071'
+    },
+    'docx': {
+        title: 'docx',
+        icon: faFileWord,
+        color: '#00b4e8',
+    },
+    'xlsx': {
+        title: 'xlsx',
+        icon: faFileExcel,
+        color: '#63e305',
+    },
+    'pptx': {
+        title: 'pptx',
+        icon: faFilePowerpoint,
+        color: '#ff6071',
+    },
+    'csv': {
+        title: 'csv',
+        icon: faFileCsv,
+        color: '#63e305',
+    },
+    'clip': {
+        title: 'clip',
+        icon: faCopy,
+        color: '#a7a6ab',
+    },
+}
---
+++ src/renderer/components/File/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
+import fileTypes from './file.types'
+
+import { File } from '../../layout/components'
+
+// eslint-disable-next-line react/display-name
+export default ({
+    // eslint-disable-next-line react/prop-types
+    extension
+}) => {
+    
+    const { icon, color, title } = fileTypes[extension] || {
+        title: 'xyz',
+        icon: faFile,
+        color: '#a7a6ab'
+    }
+
+    return (
+        <File>
+            <File.Icon color={color}>
+                <FontAwesomeIcon icon={icon} fixedWidth />
+            </File.Icon>
+            <File.Extension color={color}>{title}</File.Extension>
+        </File>
+    )
+}
