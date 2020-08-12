f392f8340e0f46363afa2001784051314197cbfa
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-08-12 12:08:53 +0200

    feat: 🎸 Adding <Card /> component

---
+++ src/renderer/components/Card/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+
+import { Card } from '../../layout/components'
+import { Tag } from '../../layout/elements'
+
+import File from '../File'
+
+// eslint-disable-next-line react/display-name
+export default () => {
+    return (
+        <Card>
+            <Card.Actions>
+                <FontAwesomeIcon icon={faEllipsisV} fixedWidth />
+            </Card.Actions>
+            <Card.Media>
+                <File extension={'pdf'} />
+            </Card.Media>
+            <Card.Title>Invoice 05/2020</Card.Title>
+            <Card.Subtitle>Added: 05-05-2020</Card.Subtitle>
+            <Card.Tags>
+                <Tag>Document</Tag>
+                <Tag>Invoices</Tag>
+                <Tag>Tag</Tag>
+                <Tag>Tag</Tag>
+                <Tag>Tag</Tag>
+            </Card.Tags>
+        </Card>
+    )
+}
