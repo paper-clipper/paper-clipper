fcbb1674271bb6db790e7129cc83b7cd09abd593
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-07 16:49:44 +0200

    fix: 🐛 Adding missing imports to layout/components

--- src/renderer/layout/components/index.js
+++ src/renderer/layout/components/index.js
@@ -1,11 +1,21 @@
 import File from './File'
 import Card from './Card'
 import Dropdown from './Dropdown'
+// import Sidebar from './Sidebar'
+// import Header from './Header'
 import Popup from './Popup'
+import Modal from './Modal'
+import TextInput from './TextInput'
+import Upload from './Upload'
 
 export {
     File,
     Card,
     Dropdown,
+    // Sidebar,
+    // Header,
     Popup,
+    Modal,
+    TextInput,
+    Upload,
 }
