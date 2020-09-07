475f41c336d9a924434479d3cf90a0e4883ba437
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-07 16:47:49 +0200

    fix: 🐛 Updating <Card /> styled-component

--- src/renderer/layout/components/Card/Media.js
+++ src/renderer/layout/components/Card/Media.js
@@ -4,6 +4,7 @@ export default styled.div`
     width: 100%;
     height: 100%;
     padding-top: 0.5rem;
+    font-size: 3.25rem;
     box-sizing: border-box;
     display: flex;
     justify-content: center;
