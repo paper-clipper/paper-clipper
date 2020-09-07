cad86bc1f8481b00ddf0d5b30bd58ff8d8cc1a34
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-07 16:39:58 +0200

    feat: 🎸 Adding <Meta /> styled-component to <Upload />

---
+++ src/renderer/layout/components/Upload/Meta.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export default styled.span`
+    font-size: 0.625rem;
+    font-family: ${({ theme }) => theme.fonts.secondary};
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.5);
+    line-height: 1.25
+`
