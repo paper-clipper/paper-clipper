20ffb5b40f6ae76f34070ed0ef7e1ca5b1d22595
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-12 15:58:10 +0200

    feat: 🎸 Adding <Button.Contained /> styled-component

--- src/renderer/layout/elements/Button.js
+++ src/renderer/layout/elements/Button.js
@@ -1,6 +1,16 @@
 import styled, { css } from 'styled-components'
 import { applyStyleModifiers } from 'styled-components-modifiers'
 
+const BUTTON_MODIFIERS = {
+
+    'icon': () => css`
+        min-width: auto;
+        width: 40px;
+        height: 40px;
+    `,
+
+}
+
 const Button = styled.button`
     display: inline-flex;
     position: relative;
@@ -15,6 +25,15 @@ const Button = styled.button`
     user-select: none;
     overflow: visible;
     cursor: pointer;
+    font-size: 0.875rem;
+    font-family: ${({ theme }) => theme.fonts.secondary};
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.black};
+    padding: 0 0.5rem;
+    min-width: 80px;
+    height: 36px;
+    border-radius: 12px;
+    ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `
 
 const TEXT_BUTTON_MODIFIERS = {
@@ -26,15 +45,27 @@ const TEXT_BUTTON_MODIFIERS = {
 }
 
 Button.Text = styled(Button)`
-    font-size: 0.875rem;
-    font-family: ${({ theme }) => theme.fonts.secondary};
-    font-weight: 700;
-    color: ${({ theme }) => theme.colors.black};
-    padding: 0 0.5rem;
-    min-width: 64px;
-    height: 36px;
-    border-radius: 12px;
     ${applyStyleModifiers(TEXT_BUTTON_MODIFIERS)};
+`
+
+const CONTAINED_BUTTON_MODIFIERS = {
+
+    'primary': ({ theme }) => css`
+        color: ${theme.colors.highlight};
+    `,
+
+}
+
+Button.Contained = styled(Button)`
+    color: ${({ theme }) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.black};
+    transition: all 200ms ease-in-out;
+
+    &:hover {
+        transform: translate(0, -2px);
+        background: rgba(0, 0, 0, 0.85);
+    }
+    ${applyStyleModifiers(CONTAINED_BUTTON_MODIFIERS)};
 `
 
 export default Button
