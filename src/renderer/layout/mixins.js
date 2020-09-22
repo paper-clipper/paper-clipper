d017f23b75544ac47d2690675da53e43ba22f942
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-22 16:59:20 +0200

    feat: 🎸 Extending mixins

--- src/renderer/layout/mixins.js
+++ src/renderer/layout/mixins.js
@@ -1,6 +1,8 @@
 import { css } from 'styled-components'
 
 export const colorPrimary = ({ theme }) => theme.newTheme.colors.primary
+export const colorPrimary80 = ({ theme }) => theme.newTheme.colors.primary80
+export const colorPrimary10 = ({ theme }) => theme.newTheme.colors.primary10
 export const colorSecondary = ({ theme }) => theme.newTheme.colors.secondary
 export const colorSecondaryDarker = ({ theme }) => theme.newTheme.colors.secondaryDarker
 export const colorNeutral = ({ theme }) => theme.newTheme.colors.neutral
@@ -14,3 +16,5 @@ export const elevation2 = ({ theme }) => css`
 
 export const fontPrimary = ({ theme }) => theme.newTheme.fonts.primary
 export const fontSecondary = ({ theme }) => theme.newTheme.fonts.secondary
+
+export const transition1 = ({ theme }) => theme.newTheme.transitions[0]
