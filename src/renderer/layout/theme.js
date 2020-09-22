9b85074cc2ac23148a26b0fff1d91a78bd654f13
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-22 15:57:51 +0200

    fix: 🐛 Cleaning theme

--- src/renderer/layout/theme.js
+++ src/renderer/layout/theme.js
@@ -14,3 +14,29 @@ export default {
         secondary: '\'Open Sans\', sans-serif',
     },
 }
+
+export const theme = {
+    colors: {
+        primary: '#000000',
+        primary50: 'rgba(0, 0, 0, 0.5)',
+        primary10: 'rgba(0, 0, 0, 0.1)',
+        secondary: '#ffffff',
+        secondaryDarker: '#f7f7f9',
+        neutral: '#a7a6ab',
+    },
+    elevations: [
+        '0 2px 16px rgba(90, 90, 90, 0.1)',
+        '0 2px 8px rgba(90, 90, 90, 0.20)',
+    ],
+    effects: {
+        backgrounBlur: [
+            'blur(50px) brightness(100%) opacity(50%)',
+            'blur(20px) brightness(50%) opacity(70%)',
+            'blur(25px) brightness(50%) opacity(50%)',
+        ],
+    },
+    fonts: {
+        main: 'Lato, sans-serif',
+        secondary: '\'Open Sans\', sans-serif',
+    },
+}
