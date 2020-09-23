6eafa6445db9941b7a072a21f608d7771395bbcb
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-23 17:54:09 +0200

    feat: 🎸 Adding Dropdown to <Card />

--- src/renderer/components/Card/index.js
+++ src/renderer/components/Card/index.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV, faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-import { Card } from '../../layout/components'
-import { Tag } from '../../layout/elements'
+import { Card } from '@paper/layout/components'
+import { Tag } from '@paper/layout/elements'
 
-import File from '../File'
+import File from '@paper/components/File'
+import Dropdown from '@paper/components/Dropdown'
+
+import { useOnClickOutside } from '@paper/hooks'
+
+const dropdownItems = [
+    {
+        name: 'Edit',
+        icon: faPen,
+        handler: () => null,
+    },
+    {
+        name: 'Delete',
+        icon: faTrash,
+        handler: () => null,
+    },
+]
 
 export default () => {
+
+    const dropdownRef = useRef()
+    const [ isDropdownOpen, setIsDropdownOpen ] = useState(false)
+    useOnClickOutside(dropdownRef, () => setIsDropdownOpen(false))
+
     return (
         <Card>
             <Card.Actions>
-                <FontAwesomeIcon icon={faEllipsisV} fixedWidth />
+                <FontAwesomeIcon
+                    icon={faEllipsisV}
+                    fixedWidth={true}
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                />
             </Card.Actions>
+            {isDropdownOpen && (
+                <Card.Dropdown>
+                    <Dropdown ref={dropdownRef} items={dropdownItems} />
+                </Card.Dropdown>
+            )}
             <Card.Media>
                 <File extension={'pdf'} showExtension={true} />
             </Card.Media>
