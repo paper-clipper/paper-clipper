5fb0092dfd3e2e5f30273ef80f38fcee5e088655
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-09-24 19:39:59 +0200

    refactor: 💡 Udpating <Tag /> styled-component

--- src/renderer/layout/elements/Tag.js
+++ src/renderer/layout/elements/Tag.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
+import {
+    fontPrimary,
+    colorPrimary,
+    colorSecondaryDarker,
+} from '@paper/layout/mixins'
 
 export default styled.span`
     display: inline-flex;
-    font-family: ${({ theme }) => theme.fonts.main};
+    font-family: ${fontPrimary};
     font-size: 0.675rem;
     font-weight: bold;
-    color: ${({ theme }) => theme.colors.black};
+    color: ${colorPrimary};
     box-sizing: border-box;
-    background: ${({ theme }) => theme.colors.whiteDarker};
+    background: ${colorSecondaryDarker};
     padding: 0.5rem 0.75rem;
     margin: 2px;
     border-radius: 5rem;
