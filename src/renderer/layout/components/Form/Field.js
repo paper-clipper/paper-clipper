7a8434435b4299a4787af73dfb8e6486e6d27dad
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-10-21 08:17:50 +0200

    feat: 🎸 Adding <Form /> styled-component

--- src/renderer/layout/components/Form/Field.js
+++ src/renderer/layout/components/Form/Field.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export default styled.div``
+export default styled.div`
+    padding: 0.25rem;
+    box-sizing: border-box;
+    width: 100%;
+    display: inline-flex;
+`
--- src/renderer/layout/components/Form/index.js
+++ src/renderer/layout/components/Form/index.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components'
 
-const Form = styled.form``
+import Field from './Field'
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`
+
+Form.Field = Field
 
 export default Form
