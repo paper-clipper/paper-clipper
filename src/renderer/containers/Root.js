f4478c72ea5c79d648375bb1c1e4fa6fe95030ab
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-11-21 16:11:11 +0100

    feat: 🎸 Adding redux

--- src/renderer/containers/Root.js
+++ src/renderer/containers/Root.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import theme from '../layout/theme'
+import { Provider } from 'react-redux'
+
+import store from '@paper/store'
+import theme from '@paper/layout/theme'
 
 import App from './App'
 
 export default () => {
     return (
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </Provider>
     )
 }
