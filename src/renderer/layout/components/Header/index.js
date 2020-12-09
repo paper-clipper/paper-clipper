17bfc314ebf084a81b53cfac3ab5d21b3d0281bf
Wojciech Skorupa <woj.skorupa@gmail.com>
2020-12-09 09:25:28 +0100

    feat: 🎸 Adding Search element to <Header /> styled-component

--- src/renderer/layout/components/Header/index.js
+++ src/renderer/layout/components/Header/index.js
@@ -4,6 +4,8 @@ import {
     backgroundBlur2,
 } from '@paper/layout/mixins'
 
+import Search from './Search'
+
 const Header = styled.header`
     width: 100%;
     height: 100%;
@@ -13,5 +15,7 @@ const Header = styled.header`
     justify-content: center;
     align-items: center;
 `
+
+Header.Search = Search
 
 export default Header
