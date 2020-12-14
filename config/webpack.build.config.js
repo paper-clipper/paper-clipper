531d16008822097a89c3f4f5065d5dc572ef3587
LamaPizama <woj.skorupa@gmail.com>
2020-12-14 16:37:08 +0100

    feat: 🎸 Adding application title to build bundle

--- config/webpack.build.config.js
+++ config/webpack.build.config.js
@@ -43,7 +43,9 @@ module.exports = {
     },
     target: 'electron-renderer',
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Paper Clipper',
+        }),
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -37,7 +37,9 @@ module.exports = {
     },
     target: 'electron-renderer',
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Paper Clipper',
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development'),
         }),
